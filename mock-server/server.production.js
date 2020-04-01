0c033f25258fec49ed2607b6b26be7cfd04492f9
sander.vergeles <sander.vergeles@gmail.com>
2020-04-01 21:02:14 +0300

    prepare mock server for production

--- mock-server/server.production.js
+++ mock-server/server.production.js
@@ -1,21 +1,36 @@
 const path = require('path')
 const express = require('express')
+const jsonServer = require('json-server')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-const app = express()
 const { MOCK_PORT, BACKEND_PORT } = process.env
-const DIST = (__dirname, '..', 'dist')
+const DIST = path.join(`${__dirname}/../dist`)
 
-app.use(express.static(path.join(DIST), { index: false, extensions: ['html'] }))
-app.use(
+const router = express.Router()
+const serverDevelopment = jsonServer.create()
+
+const v1Router = jsonServer.router('./mock-server/db.json')
+const v2Router = jsonServer.router('./mock-server/db-v2.json')
+
+router.use(
+  express.static(path.join(DIST), { index: false, extensions: ['html'] }),
+)
+router.use('/mock/api', v1Router)
+router.use('/mock/api/v2', v2Router)
+router.use(
   '/api/*',
-  createProxyMiddleware({ target: `http://localhost:${BACKEND_PORT}`, changeOrigin: true }),
+  createProxyMiddleware({
+    target: `http://localhost:${BACKEND_PORT}`,
+    changeOrigin: true,
+  }),
 )
 
-app.get('*', (req, res) => {
-  res.sendfile(path.join(DIST, 'index.html'))
+router.get('*', (req, res) => {
+  res.sendFile(`${DIST}/index.html`)
 })
+
+serverDevelopment.use('/', router)
 
 console.log('MOCK_PORT: ', MOCK_PORT)
 console.log('BACKEND_PORT: ', BACKEND_PORT)
-app.listen(MOCK_PORT)
+serverDevelopment.listen(MOCK_PORT)
